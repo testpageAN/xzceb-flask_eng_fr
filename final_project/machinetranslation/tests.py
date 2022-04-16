6694f50568fafa2271f05cef6480356f6b8024a2
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-04-16 19:33:12 +0000

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-# from translator import english_to_french, french_to_english
+#from translator import english_to_french, french_to_english
 from . import translator
 
 class Testenglish_to_french(unittest.TestCase): 
