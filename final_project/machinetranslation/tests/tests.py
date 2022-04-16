173747663237ff7261b7900a9c986f2c367a50bc
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-04-16 14:37:34 +0000

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+# from machinetranslation/translator import english_to_french, french_to_english
+#from translator import english_to_french, french_to_english
+from . import translator
+
+class Testenglish_to_french(unittest.TestCase): 
+    def test1(self): 
+        self.assertNotEqual(english_to_french('None'), '') # test null input.
+        self.assertEqual(english_to_french('Hello'), 'Bonjour') # Test for the translation of the world 'Hello' and 'Bonjour'.
+        
+
+class Testfrench_to_english(unittest.TestCase): 
+    def test1(self):
+        self.assertNotEqual(french_to_english('None'), '') # test null input.
+        self.assertEqual(french_to_english('Bonjour'), 'Hello') # Test for the translation of the world 'Bonjour' and 'Hello'.
+        
+        
+
+unittest.main()
